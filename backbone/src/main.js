9f2041dfab76979e76101432638067f753903cff
Tyrone Davis <tdavisjr@gmail.com>
2016-07-16 08:07:33 -0400

    minor requirejs config refactor

--- backbone/src/main.js
+++ backbone/src/main.js
@@ -1,13 +1,7 @@
-require.config({
-    paths: {
-        jquery: '../vendor/jquery/dist/jquery.min',
-        backbone: '../vendor/backbone/backbone',
-        underscore: '../vendor/underscore/underscore'  
-    }
-});
+require(['appView', 'users'], function(AppView, Users) {
 
-require(['appView', 'users'], function(AppView, Users){
-
-     new AppView({ collection: new Users() });
+    new AppView({
+        collection: new Users()
+    });
 
 });
