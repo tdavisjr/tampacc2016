671ca20d3124e015a0066cb1f0f1fcb45f662b32
Tyrone Davis <tdavisjr@gmail.com>
2016-10-21 17:03:51 -0400

    upgrade Vuejs to 2.0

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,36 +1,39 @@
 require([
-'vue',
-'fetch',
-'pageHeaderView',
-'loaderView',
-'usersView'
-], 
-function(Vue, fetch, PageHeaderView, LoaderView, UsersView) {
+    'vue',
+    'fetch',
+    'pageHeaderView',
+    'loaderView',
+    'usersView'
+],
+    function (Vue, fetch, PageHeaderView, LoaderView, UsersView) {
 
-    Vue.component('page-header', PageHeaderView);
-    Vue.component('loader', LoaderView);
-    Vue.component('users-list', UsersView);
+        Vue.component('page-header', PageHeaderView);
+        Vue.component('loader', LoaderView);
+        Vue.component('users-list', UsersView);
 
-    var vm = new Vue({
-        el: '#app',
-        data: {
-            isLoading: true,
-            users: []
-        },
+        var vm = new Vue({
+            el: '#app',
+            data: {
+                isLoading: true,
+                users: []
+            },
 
-        ready: function() {
-            fetch('https://api.github.com/users')
-                .then(function(response) {
-                    return response.json()
+            mounted: function () {
+                this.$nextTick(function () {
+                    fetch('https://api.github.com/users')
+                        .then(function (response) {
+                            return response.json()
+                        })
+                        .then(function (data) {
+                            this.users = data;
+                            this.isLoading = false;
+                        }.bind(this))
+                        .catch(function (ex) {
+                            console.log('parsing failed', ex)
+                        });
                 })
-                .then(function(data) {
-                    this.users = data;
-                    this.isLoading = false;
-                }.bind(this))
-                .catch(function(ex) {
-                    console.log('parsing failed', ex)
-                });
-        }
-    });
+            }
 
-});
+        });
+
+    });
